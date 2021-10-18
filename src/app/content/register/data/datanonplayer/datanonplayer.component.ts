190d3a145a986c5ecc28e2dcf688099ddc81d1d0
wendyhendra90 <wendyhendra90@gmail.com>
2021-10-18 13:55:00 +0700

    english

--- src/app/content/register/data/datanonplayer/datanonplayer.component.ts
+++ src/app/content/register/data/datanonplayer/datanonplayer.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MatTableDataSource } from '@angular/material/table';
+interface nonplayer{
+  no:number;namalengkap:string;kebangsaan:string;posisi:string;noanggotadct:string
+}
+const ELEMENT_DATA2: nonplayer[] = [
+  {no: 1, namalengkap: 'Hydrogen', kebangsaan: 'Indonesia',posisi:'Parents',noanggotadct:''},
+  {no: 2, namalengkap: 'Helium', kebangsaan: 'Indonesia', posisi:'Coord. Regional',noanggotadct:''},
+  {no: 3, namalengkap: 'Lithium', kebangsaan: 'International', posisi:'Coach',noanggotadct:''},
+  {no: 4, namalengkap: 'Beryllium', kebangsaan: 'Indonesia',posisi:'Coach',noanggotadct:''},
+];
 @Component({
   selector: 'app-datanonplayer',
   templateUrl: './datanonplayer.component.html',
   styleUrls: ['./datanonplayer.component.css']
 })
 export class DatanonplayerComponent implements OnInit {
-
+  displayedColumns: string[] = ['no', 'namalengkap', 'kebangsaan'
+  ,'posisi','noanggotadct'];
+  dataSource = new MatTableDataSource(ELEMENT_DATA2);
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  applyFilter(filtervalue:string){
+    this.dataSource.filter=filtervalue.trim().toLowerCase();
+  }
 }
