87fdabe0b3684c2c9312d7351380579307df03cc
Faris Amin <faris.amin@gmail.com>
2021-11-08 12:25:03 +0700

    Fix Download Zip File

--- src/app/content/register/data/datateam/datateam.component.ts
+++ src/app/content/register/data/datateam/datateam.component.ts
@@ -18,93 +18,98 @@ export interface datateam {
 
 
 export class DatateamComponent implements OnInit {
-  authenticated=false;
-  displayedColumns: string[] = ['id', 'team_name', 'team_classification','files','downloadfiles'];
-  response:any;
-  dataSource = new MatTableDataSource < datateam > ();
+  authenticated = false;
+  displayedColumns: string[] = ['id', 'team_name', 'team_classification', 'files', 'downloadfiles'];
+  response: any;
+  dataSource = new MatTableDataSource<datateam>();
   constructor(
-    private http:HttpClient
+    private http: HttpClient
   ) { }
-    
+
   ngOnInit(): void {
-    if(localStorage.getItem('role')=='admin'){
-      this.authenticated=true;
-    }else{
-      this.authenticated=false;
+    if (localStorage.getItem('role') == 'admin') {
+      this.authenticated = true;
+    } else {
+      this.authenticated = false;
     }
-    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist?order=team_name').subscribe((res:any)=>
-          {
-           //console.log(res);
-           this.response=res.data;
-           this.dataSource=new MatTableDataSource <datateam> (res.data.data);
+    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist?order=team_name').subscribe((res: any) => {
+      //console.log(res);
+      this.response = res.data;
+      this.dataSource = new MatTableDataSource<datateam>(res.data.data);
 
-          },(err:any)=>{
-            console.error(err);
-            alert(err)
-          })
-  }
-  isloading=false;
-  applyFilter(filtervalue:string){
-    this.isloading=true
-    //this.dataSource.filter=filtervalue.trim().toLowerCase();
-    let params=new HttpParams();
-    params=params.append('search',String(filtervalue));
-    params=params.append('filter',String('team_name'));
-    params=params.append('order',String('team_name'));
-    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist',{params}).subscribe((res:any)=>
-    {
-      this.isloading=false
-      this.response=res.data;
-           this.dataSource=new MatTableDataSource <datateam> (res.data.data);
-
-          },(err:any)=>{
-            console.error(err);
-            alert(err)
+    }, (err: any) => {
+      console.error(err);
+      alert(err)
     })
   }
-  pageEvent:PageEvent;
-  onPaginateChange(event:PageEvent){
+  isloading = false;
+  applyFilter(filtervalue: string) {
+    this.isloading = true
+    //this.dataSource.filter=filtervalue.trim().toLowerCase();
     let params = new HttpParams();
-    let page=event.pageIndex;
-    let size=event.pageSize;
-    page=page+1;
+    params = params.append('search', String(filtervalue));
+    params = params.append('filter', String('team_name'));
+    params = params.append('order', String('team_name'));
+    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist', { params }).subscribe((res: any) => {
+      this.isloading = false
+      this.response = res.data;
+      this.dataSource = new MatTableDataSource<datateam>(res.data.data);
+
+    }, (err: any) => {
+      console.error(err);
+      alert(err)
+    })
+  }
+  pageEvent: PageEvent;
+  onPaginateChange(event: PageEvent) {
+    let params = new HttpParams();
+    let page = event.pageIndex;
+    let size = event.pageSize;
+    page = page + 1;
     params = params.append('page', String(page));
     params = params.append('limit', String(size));
-    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist',{params}).subscribe((res:any)=>
-    {
-      this.response=res.data;
-           this.dataSource=new MatTableDataSource <datateam> (res.data.data);
+    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist', { params }).subscribe((res: any) => {
+      this.response = res.data;
+      this.dataSource = new MatTableDataSource<datateam>(res.data.data);
 
-          },(err:any)=>{
-            console.error(err);
-            alert(err)
+    }, (err: any) => {
+      console.error(err);
+      alert(err)
     })
   }
-  imgSrc:any;
-  downloadfiles(id:number){
-    let params=new HttpParams();
-    params=params.append('filter',String('id'));
-    params=params.append('search',id);
+  imgSrc: any;
+  downloadfiles(id: number) {
+    let params = new HttpParams();
+    params = params.append('filter', String('id'));
+    params = params.append('search', id);
 
-    this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist',
-    {params, responseType:'blob'}).subscribe((response: any) => {
-      console.log(response)
-		// 	const data=response.data.data[0].files;
-
-    //   const filename=response.data.data[0].file_name;
-      let blob:any = new Blob([response], {type:'image/png'} );//{type:'application/zip'}
-      console.log(blob)
-
-		// saveAs(blob, filename);
-    saveAs(new Blob([blob], {type:'image/png'}) ,'tes');
-		},(err: any) =>{ console.log('Error downloading the file')}
-    )
-    	//const url = window.URL.createObjectURL(blob);
-			// window.open(url);
-			// window.location.href = response.url;
+    this.http.get('https://hercules.aturtoko.id/dct/public/datateam',
+      { params }).subscribe((response: any) => {
+        const extension = response.data.data[0].file_name.split('.').pop();
+        if (extension === 'zip') {
+          const theData = atob(response.data.data[0].files);
+          let blob = new Blob([theData], { type: 'application/gzip' })
+          saveAs(this.b64toFile(response.data.data[0].files, response.data.data[0].file_name, 'application/zip'));
+        }
+      }, (err: any) => { console.log('Error downloading the file') }
+      )
   }
-  // this.http.get('https://hercules.aturtoko.id/dct/public/datateamlist',{params}).subscribe(res=> {
-    //   console.log(res)
-    //   responseType: 'arraybuffer'
-    // });
+
+  b64toFile(b64Data: any, filename: any, contentType: any) {
+    var sliceSize = 512;
+    var byteCharacters = atob(b64Data);
+    var byteArrays = [];
+    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+      var slice = byteCharacters.slice(offset, offset + sliceSize);
+      var byteNumbers = new Array(slice.length);
+      for (var i = 0; i < slice.length; i++) {
+        byteNumbers[i] = slice.charCodeAt(i);
+      }
+      var byteArray = new Uint8Array(byteNumbers);
+      byteArrays.push(byteArray);
+    }
+    var file = new File(byteArrays, filename, { type: contentType });
+    return file;
+  }
+
 }
